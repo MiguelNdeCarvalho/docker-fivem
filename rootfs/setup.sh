bb0ccbf94ac5a1f2aa0e8ec934c37cd9b70f6830
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2020-04-24 19:57:53 +0100

    setup.sh Fix some mistakes

--- rootfs/setup.sh
+++ rootfs/setup.sh
@@ -7,9 +7,11 @@ echo "Download required files and move it"
 cd /tmp
 mkdir server
 wget https://runtime.fivem.net/artifacts/fivem/build_proot_linux/master/${FIVEM_VER}/fx.tar.xz
-tar -xf fx.tar.xz -C /server
+tar -xf fx.tar.xz -C server
 git clone https://github.com/citizenfx/cfx-server-data.git server-data
 mv server /fivem
 mv server-data /fivem
 rm /tmp/*
-mv /roots/server.cfg /fivem/server-data/
+mv /server.cfg /fivem/server-data/
+
+echo "Everything is done!"
